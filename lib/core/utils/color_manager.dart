bcc5f13c088e89e53d6a9d2b38b1b50ec362ab7a
byteVortex0 <mahmodahmed201559@gmail.com>
2025-10-16 03:11:32 +0300

    onboarding done added colors, images, font style

--- lib/core/utils/color_manager.dart
+++ lib/core/utils/color_manager.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
 class ColorManager {
-  static const Color brandColor = Color(0xFF9775FA);
+  static const Color brandColor = Color(0xFF44BDB6);
+  static const Color black = Color(0xFF090909);
+  static const Color grey = Color(0xFF9F9F9F);
 }
